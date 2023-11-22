874de2afde6add60b0f1d6c940dc9dcd12663088
RedBug <miguel@kyuran.be>
2023-11-22 17:54:38 +0100

    Add files via upload

---
+++ quicksearch.lua
@@ -0,0 +1,7 @@
+-- <km.title>Quick Search</km.title>
+-- <km.version>v1.0</km.version>
+-- <km.author>Miguel Vanhove</km.author>
+-- <km.author.github>redbug26</km.author.github>
+-- <km.desc>Display the quick search panel</km.desc>
+
+km.showsearch()
